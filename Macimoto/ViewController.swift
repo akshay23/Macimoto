b2509714260ed59be461384c2d85bfee7a5398ec
akshay23 <akshay@animoto.com>
2015-12-07 08:47:04 -0500

    Initial Commit

---
+++ Macimoto/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Macimoto
+//
+//  Created by Akshay Bharath on 12/7/15.
+//  Copyright © 2015 Akshay Bharath. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: AnyObject? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
