3fdcdf58af1efc7623de7fed9c8b73de6fd92231
akshay23 <akshay@animoto.com>
2015-12-07 11:58:14 -0500

    Initial version of the login ui

--- Macimoto/ViewController.swift
+++ Macimoto/ViewController.swift
@@ -10,10 +10,24 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+  @IBOutlet var header: NSView!
+  @IBOutlet var subHeader: NSView!
+  @IBOutlet var credentialsView: NSView!
+
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    // Do any additional setup after loading the view.
+    // Make BG of sub header black
+    subHeader.wantsLayer = true
+    let subHeaderLayer = subHeader.layer
+    subHeaderLayer?.backgroundColor = CGColorCreateGenericRGB(0, 0, 0, 1)
+    
+    // Change BG color of credentials view
+    credentialsView.wantsLayer = true
+    let credentialsLayer = credentialsView.layer
+    credentialsLayer?.borderWidth = 1
+    credentialsLayer?.borderColor = CGColorCreateGenericRGB(0, 0, 0, 1)
+    credentialsLayer?.backgroundColor = CGColorCreateGenericRGB(255, 255, 255, 1)
   }
 
   override var representedObject: AnyObject? {
