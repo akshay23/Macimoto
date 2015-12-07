2e50badce11c1c12550e403cf29fd9822bdd2af9
akshay23 <akshay@animoto.com>
2015-12-07 13:13:32 -0500

    Update the look of the log in button

--- Macimoto/ViewController.swift
+++ Macimoto/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: NSViewController {
   @IBOutlet var header: NSView!
   @IBOutlet var subHeader: NSView!
   @IBOutlet var credentialsView: NSView!
+  @IBOutlet var usernameTxt: NSTextField!
+  @IBOutlet var passwordTxt: NSSecureTextField!
+  @IBOutlet var loginButton: NSButton!
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -28,6 +31,11 @@ class ViewController: NSViewController {
     credentialsLayer?.borderWidth = 1
     credentialsLayer?.borderColor = CGColorCreateGenericRGB(0, 0, 0, 1)
     credentialsLayer?.backgroundColor = CGColorCreateGenericRGB(255, 255, 255, 1)
+    
+    // Change color of the login button text
+    let pstyle = NSMutableParagraphStyle()
+    pstyle.alignment = NSTextAlignment.Center
+    loginButton.attributedTitle = NSAttributedString(string: "Log In", attributes: [ NSForegroundColorAttributeName : NSColor.whiteColor(), NSParagraphStyleAttributeName : pstyle, NSFontAttributeName : NSFont.systemFontOfSize(16) ])
   }
 
   override var representedObject: AnyObject? {
